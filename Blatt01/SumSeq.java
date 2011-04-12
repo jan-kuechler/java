f01d108e86600f806da0677d7721a4d1f0de7864
Jan <jan@kuechler-wuppertal.de>
2011-04-12 17:11:04 +0200

    B01-A1: Minimal style changes

--- Blatt01/SumSeq.java
+++ Blatt01/SumSeq.java
@@ -5,44 +5,36 @@ class SumSeq
 	public static void main(String args[])
 	{
 		// Check arguments
-		if (args.length == 0)
-		{
+		if (args.length == 0) {
 			System.out.println("Usage: java SumSeq <int> [<int> ...]");
 			return;
 		}
-
 
 		int prev = 0;   // Previous number
 		int count = 0;  // 'prev' occured 'count' times
 		int sum = 0;    // Total sum of all parameters
 
 		// Iterate through all parameters
-		for (int i=0; i < args.length; i++)
-		{
-			Scanner sc = new Scanner(args[i]);
+		for (String a : args) {
+			Scanner sc = new Scanner(a);
 
 			// Check for valid integers
-			if (!sc.hasNextInt())
-			{
-				System.out.printf("Error: '%s' is not an integer.\n", args[i]);
-				return;
+			if (!sc.hasNextInt()) {
+				System.out.printf("Error: '%s' is not an integer.\n", a);
+				continue;
 			}
 
 			int n = sc.nextInt();
 
 			// This is a new number, output the count (if any) of the previous one.
-			if (n != prev)
-			{
+			if (n != prev) {
 				if (count > 0)
-				{
 					System.out.printf("%d mal die Zahl %d\n", count, prev);
-				}
 
 				prev = n;
 				count = 1;
 			}
-			else
-			{
+			else {
 				// Nothing new, just increment the count
 				count++;
 			}
@@ -52,9 +44,8 @@ class SumSeq
 
 		// Output the last sequence...
 		if (count > 0)
-		{
 			System.out.printf("%d mal die Zahl %d\n", count, prev);
-		}
+
 		// ... and the sum
 		System.out.printf("Summe: %d\n", sum);
 	}
